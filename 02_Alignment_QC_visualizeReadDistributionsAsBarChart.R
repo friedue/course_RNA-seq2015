1b6dbf6298936edda6da51767bf959379e491851
Friederike Duendar <frd2007@med.cornell.edu>
2015-09-25 14:29:26 -0400

    additional scripts for RNA-seq class

---
+++ 02_Alignment_QC_visualizeReadDistributionsAsBarChart.R
@@ -0,0 +1,42 @@
+## get output of RSeQC's read_distribution.py
+## to run this script as is, save the example .txt files in your current working directory
+
+## get all file names
+files <- list.files(pattern = "readDistribution_UHR", full.names = TRUE)
+
+## source the following function
+ReadIn_ReadDistrbution <- function(FileName){
+  # This function takes the output that is printed to the screen by RSeQC's read_distribution.py 
+  # If samples and replicates should be filled in as well,
+  # this function expects that the RSeQC output was parsed to a .txt file
+  # named readDistribution_SAMPLENAME_REPLICATENUMBER.txt
+  # e.g.: read_distribution.py -i ${BAM} -r referenceGenomes/human/hg19_RefSeq.bed > readDistribution_${SAMPLENAME}_${REPLICATE}.txt
+  
+  in.df <- read.table(FileName, skip = 4, stringsAsFactors = FALSE, fill=TRUE, header=TRUE)[c("Group", "Tags.Kb")]
+  in.df <- in.df[complete.cases(in.df),]
+  in.df <- transform(in.df,
+                     Sample = gsub(".*readDistribution_([a-zA-Z0-9]*)[_-]*([A-Z0-9]*).*\\.txt","\\1", FileName),
+                     Replicate = gsub(".*readDistribution_([a-zA-Z0-9]*)[_-]([A-Z0-9]*).*\\.txt","\\2",FileName)
+  )
+  return(in.df)
+}
+
+## read in all files (produces a list of data frames)
+readDistribution <- lapply(files, function(x) ReadIn_ReadDistrbution(x))
+
+## concatenate all data.frames from the list into one big data.frame
+readDistribution <- as.data.frame(do.call(rbind, readDistribution))
+
+## make the plot
+library(ggplot2)
+
+P <- ggplot(data = subset(readDistribution, Tags.Kb > 10),
+       aes(x=Group, y=Tags.Kb, fill = Replicate)) +
+    geom_bar(stat="identity", position = position_dodge()) +
+    theme_bw(base_size = 14) +
+    theme(legend.position="bottom") +
+    coord_flip() +
+    ylab("Tags per kb") +  ggtitle("Tags per kb > 10") + xlab("") +
+    scale_fill_manual(values = c("forestgreen","darkseagreen","green","limegreen"))
+
+print(P)
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# course_RNA-seq2015
+# RNA-seq course by Applied Bioinformatics Core, WCMC
+
+Here, you can find some additional scripts that go beyond the course notes. 
+
+### 1. Visualize the STAR alignment information (advanced R)
+
+We aligned 2x48 yeast samples (2 conditions: WT and SNF2) and wanted to compare the results of the STAR alignment across all 96 samples.
+The result can be seen in the pdf file, which can be recreated by running the 01_Alignment_visualizeSTARresults.Rmd file in RStudio.
+You can find all the functions and commands used to:
+
+* read in the STAR log file
+* combine the log files for several samples
+* make data.frames suitable for ggplot2 plotting
+* combine various ggplot2 figures into one image
+
+
+### 2. Visualize the output of multiple RSeQC read_distribution.py runs
+
+This is a simpler exercise than the STAR log file visualization.
+
+If you want to reproduce the figure 02_barplot_readDistributions.png, download the .txt files from the folder 02_readDistribution_input and carry out the steps detailed in the .R script.
